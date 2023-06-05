651c913accd278ac846306df411831a1f3282d12
R0main123 <eloro122@gmail.com>
2023-06-05 13:11:22 +0200

    Flask begining

--- app.py
+++ app.py
@@ -3,12 +3,72 @@ from tkinterdnd2 import TkinterDnD
 from excel import Excel_IV, Excel_JV
 from init_BD import create_db
 from plot_and_powerpoint import PowerPoint_IV, PowerPoint_JV
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+all_files=[]
 
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    files = request.files.getlist('file[]')
+
+    for file in files:
+        print(f"File name: {file.filename}")
+        all_files.append(file)
+    print(all_files)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port = 5000)
--- static/script.js
+++ static/script.js
@@ -1,3 +1,43 @@
 function changeMessage(){
     document.getElementById('welcome-message').textContent="Le message a changé!";
 }
+
+function handleFileSelect(evt){
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files;
+
+    for(var i=0, f; f=files[i]; i++){
+        formData.append('file[]',f,f.name);
+    }
+
+    fetch('/upload',{
+        method: "POST",
+        body:formData
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    }).then(data => {
+        console.log(data);
+    }).catch(e=>{
+        console.log('There was a problem with the fetch operation: '+e.message);
+    });
+}
+
+function handleDragOver(evt){
+    evt.preventDefault();
+    evt.stopPropagation();
+    evt.dataTransfer.dropEffect='send';
+}
+
+var dropZone = document.getElementById("drop_zone");
+dropZone.addEventListener("dragover", handleDragOver,false)
+dropZone.addEventListener("drop", handleFileSelect,false)
+
+
+
+
+
