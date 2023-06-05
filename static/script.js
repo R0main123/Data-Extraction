26322b9d091a96e285a69cc6a666dff0a5fa41eb
LAUP Romain <romain.laup@telecomnancy.eu>
2023-06-05 16:27:33 +0200

    Js fix

--- static/script.js
+++ static/script.js
@@ -1,47 +1,31 @@
-function changeMessage(){
-    document.getElementById('welcome-message').textContent="Le message a changé!";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    let dropZone = document.getElementById('drop_zone');
 
-document.addEventListener("DOMContentLoaded", function() {
-    function handleFileSelect(evt){
-    evt.stopPropagation();
-    evt.preventDefault();
+    // Prevent default behavior when file is dragged over drop zone
+    dropZone.ondragover = function(event) {
+        event.preventDefault();
+    };
 
-    var files = evt.dataTransfer.files;
-    var formData = new FormData();  // N'oubliez pas de définir formData
+    // Handle dropped files
+    dropZone.ondrop = function(event) {
+        event.preventDefault();
 
-    for(var i=0, f; f=files[i]; i++){
-        formData.append('file[]',f,f.name);
-    }
+        let files = event.dataTransfer.files;
 
-    fetch('/upload', {
-    method: "POST",
-    body: formData,
-}).then(response => {
-    if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.text();
-}).then(url => {
-    window.location.href = url;  // Redirige vers l'URL renvoyée par le serveur
-}).catch(e => {
-    console.log('There was a problem with the fetch operation: ' + e.message);
+        let formData = new FormData();
+        for (let i = 0; i < files.length; i++) {
+            formData.append('file', files[i]);
+        }
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            if (response.ok) {
+                window.location = "/options";
+            } else {
+                console.error('Upload failed');
+            }
+        });
+    };
 });
-
-}
-
-
-    function handleDragOver(evt){
-        evt.preventDefault();
-        evt.stopPropagation();
-        evt.dataTransfer.dropEffect='send';
-    }
-
-    var dropZone = document.getElementById("drop_zone");
-    dropZone.addEventListener("dragover", handleDragOver,false)
-    dropZone.addEventListener("drop", handleFileSelect,false)
-});
-
-
-
-
