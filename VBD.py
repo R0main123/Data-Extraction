032b813715eec2b917a21f732b39890582f47a13
R0main123 <eloro122@gmail.com>
2023-06-27 09:25:23 +0200

    Fix plot wafermap

--- VBD.py
+++ VBD.py
@@ -5,6 +5,26 @@ from pymongo import MongoClient
 import seaborn as sns
 from plot_and_powerpoint import fig_to_base64
 
+
+def get_compliance(wafer_id=str, structure_id=str):
+    """
+        This function finds the compliance from the specified structure in the database
+        Returns None if the structure has no compliance registered
+        :param <str> wafer_id: name of the wafer_id
+        :param <str> structure_id: name of the structure
+        :return <str>: the compliance in the wafer
+    """
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['Measurements']
+    collection = db["Wafers"]
+
+    wafer = collection.find_one({"wafer_id": wafer_id})
+    for structure in wafer["structures"]:
+        if structure["structure_id"] == structure_id:
+            return structure.get("compliance")
+
+    return None
+
 def calculate_breakdown(X, Y, compliance=0.001):
     """
     This functions calculates Voltage breakdown for two vectors given, and a compliance. Default value of the compliance is set to 1e-3
@@ -153,11 +173,14 @@ def get_vectors_in_matrix(wafer_id, structure_id, x, y):
 def create_wafer_map(wafer_id, structure_id, compliance=None):
     """
     This function displays a wafer map from a wafer, a structure and a compliance. Zeros are display in white and other values are from Blue to red, like a heatmap
+    By default, compliance is the one registered in the database. If there is not or if tis compliance is never reached, default value is 1e-3.
     :param <str> wafer_id: the name of the wafer
     :param <str> structure_id: the name of the structure
 
     :return <list>: List of x in the structure
     """
+    if compliance is None:
+        compliance = get_compliance(wafer_id, structure_id)
 
     X = np.array(get_all_x(wafer_id, structure_id))
     Y = np.array(get_all_y(wafer_id, structure_id))
@@ -185,7 +208,61 @@ def create_wafer_map(wafer_id, structure_id, compliance=None):
                     VBDs[int(float(matrix["coordinates"]["y"]) - min_y), int(float(matrix["coordinates"]["x"]) - min_x)] = VBD
 
                 else:
-                    VBDs[int(float(matrix["coordinates"]["y"]) - min_y), int(float(matrix["coordinates"]["x"]) - min_x)] = np.nan
+                    VBDs[int(float(matrix["coordinates"]["y"]) - min_y), int(float(matrix["coordinates"]["x"]) - min_x)] = 0
+    print(VBDs)
+
+    if np.all(VBDs == 0):
+        return(create_wafer_map(wafer_id, structure_id, compliance=1e-3))
+
+    else:
+        VBDs[VBDs == 0] = np.nan
+
+        x_ticks = np.linspace(min_x, max_x, VBDs.shape[1])
+        y_ticks = np.linspace(min_y, max_y, VBDs.shape[0])
+
+        cmap = colors.LinearSegmentedColormap.from_list(
+            "mycmap", [(0, "blue"), (1, "red")]
+        )
+
+        sns.heatmap(VBDs, cmap=cmap)
+
+        x_interval = max(1, int(len_x/5))
+        y_interval = max(1, int(len_y/5))
+
+        plt.xticks(np.arange(0, VBDs.shape[1], x_interval), x_ticks[::x_interval])
+        plt.yticks(np.arange(0, VBDs.shape[0], y_interval), y_ticks[::y_interval])
+
+        plt.title(f"{structure_id} in {wafer_id}. Compliance: {1e-3 if compliance is None else compliance}", pad=30)
+
+        fig = plt.gcf()
+
+        base64_str = fig_to_base64(fig)
+
+        plt.close(fig)
+
+        return base64_str
+
+def create_personal_wafer_map(wafer_id, structure_id, compliances):
+    """
+    This function displays a wafer map from a wafer, a structure and a compliance. Zeros are display in white and other values are from Blue to red, like a heatmap
+    :param <str> wafer_id: the name of the wafer
+    :param <str> structure_id: the name of the structure
+
+    :return <list>: List of x in the structure
+    """
+
+    X = np.array(get_all_x(wafer_id, structure_id))
+    Y = np.array(get_all_y(wafer_id, structure_id))
+
+    min_x, max_x, min_y, max_y = np.min(X), np.max(X), np.min(Y), np.max(Y)
+
+    len_x = int(max_x - min_x + 1)
+    len_y = int(max_y - min_y + 1)
+
+    VBDs = np.zeros((len_y, len_x))
+
+    for x, y, compliance in compliances:
+        VBDs[y - min_y, x - min_x] = compliance
 
     VBDs[VBDs == 0] = np.nan
 
@@ -213,8 +290,6 @@ def create_wafer_map(wafer_id, structure_id, compliance=None):
     plt.close(fig)
 
     return base64_str
-
-
 
 
 
